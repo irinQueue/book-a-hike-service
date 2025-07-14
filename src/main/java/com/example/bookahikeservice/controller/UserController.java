5854a54b7b7e72b6b16aa133c9f9225d060ed328
irinQueue <irincojohnico@gmail.com>
2025-07-14 12:15:31 +0800

    feat/added logger

--- src/main/java/com/example/bookahikeservice/controller/UserController.java
+++ src/main/java/com/example/bookahikeservice/controller/UserController.java
@@ -6,17 +6,22 @@ import com.example.bookahikeservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.util.List;
 
 @RestController
 public class UserController {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
     @Autowired
     private UserService userService;
 
     @GetMapping("/api/user")
     public List<User> getUser() {
-        return userService.getUser();
+        logger.info("GET /api/user called");
+        List<User> users = userService.getUser();
+        logger.debug("Retrieved {} users", users.size());
+        return users;
     }
 }
