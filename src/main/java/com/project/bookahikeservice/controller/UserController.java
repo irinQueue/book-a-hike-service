afc979716479036f7cde822dd05a1c75abb99d7b
irinQueue <irincojohnico@gmail.com>
2025-07-14 22:19:52 +0800

    feat/ added postmapping or post create user

--- src/main/java/com/project/bookahikeservice/controller/UserController.java
+++ src/main/java/com/project/bookahikeservice/controller/UserController.java
@@ -4,20 +4,24 @@ package com.project.bookahikeservice.controller;
 import com.project.bookahikeservice.entity.User;
 import com.project.bookahikeservice.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/user")
 public class UserController {
 
 
     @Autowired
     private UserService userService;
 
-    @GetMapping("/api/user")
+    @GetMapping()
     public List<User> getUser() {
         return userService.getUser();
+    }
+    @PostMapping("/create-user")
+    public List<User> createUser(@RequestBody User user) {
+        return userService.saveUser(user);
     }
 }
--- src/main/java/com/project/bookahikeservice/service/impl/UserServiceImpl.java
+++ src/main/java/com/project/bookahikeservice/service/impl/UserServiceImpl.java
@@ -2,12 +2,13 @@ package com.project.bookahikeservice.service.impl;
 
 import com.project.bookahikeservice.controller.UserController;
 import com.project.bookahikeservice.entity.User;
+import com.project.bookahikeservice.repository.UserRepository;
 import com.project.bookahikeservice.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.project.bookahikeservice.repository.UserRepository;
+
 import java.util.List;
 
 @Service
@@ -19,9 +20,17 @@ public class UserServiceImpl implements UserService {
     private  UserRepository userRepository;
 
     @Override
+    public List<User> saveUser(User user) {
+        logger.info("POST /api/user/create-user called");
+        logger.info("User {} has been added", user);
+        userRepository.save(user);
+        return userRepository.findAll();
+    }
+
+    @Override
     public List<User> getUser() {
-        logger.info("GET /api/user called");
         List<User> users = userRepository.findAll();
+        logger.info("GET /api/user called");
         logger.info("Fetched {} users from the database", users.size());
         return users;
     }
