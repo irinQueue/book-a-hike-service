fe828bc7b83f930e5469730f4b74c6d0f2203eb9
irinQueue <johnicoirinco.hs@gmail.com>
2025-08-03 22:36:23 +0800

    feat added image uploading on creating and updating an event

--- src/main/java/com/project/bookahikeservice/repository/EventRepository.java
+++ src/main/java/com/project/bookahikeservice/repository/EventRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.domain.Pageable;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface EventRepository extends JpaRepository<Event, Long> {
+public interface EventRepository extends JpaRepository<Event, UUID> {
     Optional<Event> findByIdOrderByCreatedAt(UUID eventId);
     Page<Event> findAllByActiveTrue(Pageable pageable);
     Page<Event> findAllByActiveFalse(Pageable pageable);
--- src/main/java/com/project/bookahikeservice/service/EventService.java
+++ src/main/java/com/project/bookahikeservice/service/EventService.java
@@ -5,14 +5,15 @@ import com.project.bookahikeservice.dto.response.EventResponseDto;
 import com.project.bookahikeservice.entity.EventBatch;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.UUID;
 
 public interface EventService {
-    EventResponseDto createEvent(EventRequestDto dto);
+    EventResponseDto createEvent(EventRequestDto dto, MultipartFile[] images);
     Page<EventResponseDto> getAllEvents(Pageable pageable);
     EventResponseDto getEventById(UUID id);
-    EventResponseDto updateEvent(UUID id, EventRequestDto dto);
+    EventResponseDto updateEvent(UUID id, EventRequestDto dto, MultipartFile[] images);
     String disableEvent(UUID id);
     String deleteEvent(UUID id);
     Page<EventResponseDto> getAllActiveEvents(Pageable pageable);
